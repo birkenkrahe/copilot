4f721a0d66b29fcbdc037a332cc912c43d834363
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2023-07-30 01:39:51 +0000

    step 2 from ChatGPT

--- fun_with_copilot/nfl_data.py
+++ fun_with_copilot/nfl_data.py
@@ -3,7 +3,7 @@ Open the csv file called nfl_offensive_stats.csv,
 then read in the csv data from the file
 """
 import csv
-with open('fun_with_copilot/nfl_offensive_stats.csv','r',encoding="utf-8") as f:
+with open('fun_with_copilot/data/nfl_offensive_stats.csv','r',encoding="utf-8") as f:
     nfl_reader = csv.reader(f)
     nfl_data = list(nfl_reader)
     print(nfl_data[0])
---
+++ fun_with_copilot/nfl_data2.py
@@ -0,0 +1,31 @@
+'''
+Code obtained from ChatGPT/GPT-4/Code Interpreter:
+https://shareg.pt/h1hmCjr
+'''
+
+import pandas as pd
+
+# Read the CSV file
+nfl_data = pd.read_csv('fun_with_copilot/data/nfl_offensive_stats.csv')
+nfl_data.head()
+
+# Convert the DataFrame to a list of dictionaries, each representing a row
+data = nfl_data.to_dict('records')
+data[:5]  # Display the first 5 rows
+
+# Convert the DataFrame to a list of dictionaries, each representing a row
+data = nfl_data.to_dict('records')
+data[:5]  # Display the first 5 rows
+
+# Initialize a dictionary to store the passing yards for each quarterback
+qb_passing_yards = {}
+
+# Loop over the rows in the data
+for row in data:
+    # Check if the player's position is "QB"
+    if row['position '] == 'QB':
+        # If the player is not yet in the dictionary, add them with their passing yards
+        # If they are already in the dictionary, add their passing yards to their current total
+        qb_passing_yards[row['player']] = qb_passing_yards.get(row['player'], 0) + row['pass_yds']
+
+print(qb_passing_yards)
